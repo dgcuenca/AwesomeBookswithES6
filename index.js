11743968f972094ce80ba3f9012ab10f13d36aa9
diego <dgcuenca@gmail.com>
2022-12-06 17:14:57 -0500

    Link clock in NavBar

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { addLibrary, quitLibrary } from './modules/script.js';
 import { displayBookList, displayAddBook, displayContact } from './modules/navbar.js';
+import Date from './modules/date.js';
 addLibrary();
 quitLibrary();
+setInterval(Date, 10);
